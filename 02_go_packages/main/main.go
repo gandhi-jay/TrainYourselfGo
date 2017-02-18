a539ba179871c8ff483675060b28d23266f71121
JayGandhi <j-gandhi@live.in>
2017-02-18 13:22:30 +0530

    Packages

---
+++ 02_go_packages/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jay-gandhi/TrainYourselfGo/02_go_packages/stringutil"
+	"github.com/jay-gandhi/TrainYourselfGo/02_go_packages/icomefromalaska"
+	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
