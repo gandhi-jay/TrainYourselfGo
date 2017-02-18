4eab382d7a8907c94309c18ab1c288c0526bb60a
JayGandhi <j-gandhi@live.in>
2017-02-18 12:13:19 +0530

    Format Printing

--- 01_go_helloworld/main.go
+++ 01_go_helloworld/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, First GO")
+	fmt.Println("Hello World, First GO!!!")
+	fmt.Println(21)
+	fmt.Printf("%d - %b \n", 52, 52)
+	fmt.Printf("%d - %b - %x \n", 79,79,79)
+	fmt.Printf("%#X \n",79)
+	for i:=65;i<125  ;i++  {
+		fmt.Printf("%d \t %b \t %#X \t %q \n",i,i,i,i)
+	}
 }
