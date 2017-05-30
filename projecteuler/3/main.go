40a42bb29de049623a7b4fc8bc28372a7bce1a6d
Jay Gandhi <jay@discoverdollar.com>
2017-05-30 16:36:52 +0530

    Description added for projecteuler

--- projecteuler/3/main.go
+++ projecteuler/3/main.go
@@ -26,3 +26,8 @@ func lagestPrimeFactor(n int) int {
 	}
 	return ans[len(ans)-1]
 }
+
+// Description
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+// Answer : 6857
