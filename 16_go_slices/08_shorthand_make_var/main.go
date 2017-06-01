eaa5e76c8eaa7f7f5295e41c42ac350abb62f14f
Jay Gandhi <jay@discoverdollar.com>
2017-06-01 17:44:10 +0530

    Slice - multidimensional slice and multi method to use slice

---
+++ 16_go_slices/08_shorthand_make_var/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	transactions := make([][]int, 0, 3)
+	for i := 0; i < 3; i++ {
+		transaction := make([]int, 0, 4)
+		for j := 0; j < 4; j++ {
+			transaction = append(transaction, i+j)
+		}
+		transactions = append(transactions, transaction)
+	}
+	fmt.Println(transactions)
+}
