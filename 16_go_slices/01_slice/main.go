cc7970ebb4bbe39284101330e70b936ee74d01c0
Jay Gandhi <jay@discoverdollar.com>
2017-06-01 17:32:03 +0530

    Slice basics

---
+++ 16_go_slices/01_slice/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Refer - https://gobyexample.com/slices
+	mySlice := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("%T\n", mySlice)
+	fmt.Println(mySlice)
+}
