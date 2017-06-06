33d4bdad284c7bc5fe04303a077b4a03bcce0e07
Jay Gandhi <jkgandhi92@gmail.com>
2017-06-06 16:49:33 +0530

    Added hash words

---
+++ 18_go_hashtable/01_hash_words/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	words := make(map[string]string)
+
+	sc := bufio.NewScanner(res.Body)
+	sc.Split(bufio.ScanWords)
+
+	for sc.Scan() {
+		words[sc.Text()] = ""
+	}
+
+	if err := sc.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Reading input:", err)
+	}
+
+	i := 0
+	for k := range words {
+		fmt.Println(k)
+		if i > 200 {
+			break
+		}
+		i++
+	}
+
+	// bs, _ := ioutil.ReadAll(res.Body)
+	// str := string(bs)
+	// fmt.Println(str)
+}
