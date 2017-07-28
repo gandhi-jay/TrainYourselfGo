8d79b147aad07c0298a2fa33187b4e0173861c8e
jay-gandhi <j-gandhi@live.in>
2017-07-29 04:41:55 +0530

    Updated Presentation.

--- 01_go_helloworld/for_loop/main.go
+++ 01_go_helloworld/for_loop/main.go
@@ -7,5 +7,6 @@ func main() {
 	// %#X will display 0xXXX hex. %q represent equivalent utf-8 character
 	for i := 0; i <= 100; i++ {
 		fmt.Printf(" %d \t %b \t %x \t %#X \t %q \n",i,i,i,i,i)
+		// 100     1100100         64      0X64    'd'
 	}
 }
