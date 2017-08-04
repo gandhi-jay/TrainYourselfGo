d8df9b49c4917c6aa4210bc219b0b96b16094f52
Gandhi Jay <jkgandhi92@gmail.com>
2017-08-04 11:49:02 +0530

    Added v2 practices

--- 01_go_helloworld/main.go
+++ 01_go_helloworld/main.go
@@ -6,9 +6,9 @@ func main() {
 	fmt.Println("Hello World, First GO!!!")
 	fmt.Println(21)
 	fmt.Printf("%d - %b \n", 52, 52)
-	fmt.Printf("%d - %b - %x \n", 79,79,79)
-	fmt.Printf("%#X \n",79)
-	for i:=65;i<125  ;i++  {
-		fmt.Printf("%d \t %b \t %#X \t %q \n",i,i,i,i)
+	fmt.Printf("%d - %b - %x \n", 79, 79, 79)
+	fmt.Printf("%#X \n", 79)
+	for i := 65; i < 125; i++ {
+		fmt.Printf("%d \t %b \t %#X \t %q \n", i, i, i, i)
 	}
 }
