fb86de70b55a7876e32316831dc95d8821f4c465
Jay Gandhi <jkgandhi92@gmail.com>
2017-12-02 05:12:05 +0530

    Added function of newDeck

--- go_stephen_grider/code/03_cards_deck/deck.go
+++ go_stephen_grider/code/03_cards_deck/deck.go
@@ -8,8 +8,24 @@ import (
 // which is a slice of string.
 type deck []string
 
+// Any variable with Type deck in app
+// can call this variable.
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Diamonds", "Spades", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			cards = append(cards, cardValue+" of "+cardSuit)
+		}
+	}
+
+	return cards
 }
