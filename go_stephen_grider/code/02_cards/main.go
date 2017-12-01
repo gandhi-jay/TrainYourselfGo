ec6e4bb3596a2091a43593cb6270be2e6475a89e
Jay Gandhi <jkgandhi92@gmail.com>
2017-12-02 04:20:14 +0530

    added Func with return type

--- go_stephen_grider/code/02_cards/main.go
+++ go_stephen_grider/code/02_cards/main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	// card := "Five of Diamonds" Error
+	card = "Five of Diamonds"
+
+	// Calling a method
+	card = newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
