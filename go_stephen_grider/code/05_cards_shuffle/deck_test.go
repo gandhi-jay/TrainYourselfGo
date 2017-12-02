dc7448c3a4519aea802f16f43dc9e686724d2138
Jay Gandhi <jkgandhi92@gmail.com>
2017-12-02 20:35:33 +0530

    added few test cases.

---
+++ go_stephen_grider/code/05_cards_shuffle/deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected length: 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card: Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card: King of Clubs, but got %v", d[len(d)-1])
+	}
+
+	if d[37] != "Queen of Hearts" {
+		t.Errorf("Expected 38th card: Queen of Hearts, but got %v", d[37])
+	}
+}
+
+func TestDeal(t *testing.T) {
+	d := newDeck()
+	hand, remainingDeck := deal(d, 5)
+
+	if len(hand) != 5 {
+		t.Errorf("Expected length of hand: 5, but got %v", len(hand))
+	}
+
+	if (len(d) - len(hand)) != len(remainingDeck) {
+		t.Errorf("Length of remaining should be remaining length(%v), but it's not equal(%v).", (len(d) - len(hand)), len(remainingDeck))
+	}
+}
