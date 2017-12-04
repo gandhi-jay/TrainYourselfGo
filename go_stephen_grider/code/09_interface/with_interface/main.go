3e1fffa4e7129d77ca96a4638e316438ae46155f
Jay Gandhi <jkgandhi92@gmail.com>
2017-12-05 00:01:06 +0530

    Interface example

---
+++ go_stephen_grider/code/09_interface/with_interface/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct {
+}
+
+type spanishBot struct {
+}
+
+type gujaratiBot struct {
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	gb := gujaratiBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+	printGreeting(gb)
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello, There"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func (gujaratiBot) getGreeting() string {
+	return "જય ગાંધી"
+}
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
